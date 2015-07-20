951b33919384089acd50cbec3e992595471bb831
joe hobson <joe@navigationnorth.com>
2015-07-19 22:09:38 -0500

    update plugin description and version

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
-=== Wordpress Content Mirroring Tool ===
+# WordPress Content Mirroring Tool #
 
+Contributors: Navigation North
+Tags: mirror, importer, external content
+Requires at least: 3.0.1
+Tested up to: 4.2.2
+Stable tag: 4.2
 License: GPLv2 or later
-
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-WordPress External Content Mirroring Tool is developed by Navigation North.
+## Copyright / License / Funding Notice
+
+Copyright (c) 2015 [Navigation North](http://navigationnorth.com), released under [GPLv3](LICENSE)
+
+This project has been funded at least or in part with Federal funds from the U.S. Department of Education under Contract Number ED-OOS-14-P-5009. The content of this publication does not necessarily reflect the views or policies of the U.S. Department of Education nor does mention of trade names, commercial products, or organizations imply endorsement by the U.S. Government.
+
+### Description ###
+
+Automatically import HTML content from external web pages and append to existing WP pages.
+
+### Frequently Asked Questions ###
+
+No Frequently Asked Questions
+
+### Screenshots ###
+
+No screenshots available.
+
+### Changelog ###
+
+= 0.8.0 =
+* Initial release, for use on innovation.ed.gov
--- oii-external-content-importer.php
+++ oii-external-content-importer.php
@@ -1,13 +1,28 @@
 <?php
-/**
- * Plugin Name: OII External Content Importer
- * Plugin URI:
- * Description: Plugin for OII External Content Imports
- * Version: 1.0.0
- * Author:
- * Author URI:
- * License:
+/*
+ Plugin Name: OII External Content Mirror
+ Plugin URI: http://www.navigationnorth.com/wordpress/wp-content-mirror
+ Description: Automatically import HTML content from external web pages and append to existing WP pages.
+ Version: 0.8.0
+ Author: Navigation North
+ Author URI: http://www.navigationnorth.com
+
+ Copyright (C) 2015 Navigation North
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 define("OII_ECI_PATH", plugin_dir_path(__FILE__));
 define("OII_ECI_URL", plugin_dir_url(__FILE__));
 define("OII_ECI_PLUGIN_DOMAIN", "oii-external-content-importer");
