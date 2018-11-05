07098024bb9c0b54dcc80e5910bc07fc4e7af1ab
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2018-11-05 22:37:07 +0800

    deactivate active configs and save appended content and meta data

--- classes/oii-eci-helper.php
+++ classes/oii-eci-helper.php
@@ -18,9 +18,11 @@ class OII_ECI_Helper {
     }
     
     public function migrate_external_content($post_id) {
+        
         $this->post_id = $post_id;
         $this->sync_page();
-        $acontents = $this->append_contents();
+        
+        $acontents = $this->appended_contents();
         
         $post = get_post($post_id);
         $content = $post->post_content;
@@ -30,6 +32,9 @@ class OII_ECI_Helper {
                       'post_content' => $content.$acontents
                       );
         
+        $this->deactivate_external_configs();
+        
+        // Saving updated content with appended external contents        
         $pId = wp_update_post($post, true);
         
         if (is_wp_error($pId)){
@@ -44,7 +49,7 @@ class OII_ECI_Helper {
         }
     }
 
-    public function sync_page() {
+    private function sync_page() {
         
         $_option = get_option(OII_ECI_Settings_Page::$option_name);
         $this->_debug = $_option["debug"];
@@ -65,7 +70,7 @@ class OII_ECI_Helper {
         
     }
     
-    public function append_contents( ) {
+    private function appended_contents( ) {
         $content = null;
         
         if (count($this->contents)>0){
@@ -84,6 +89,21 @@ class OII_ECI_Helper {
         return $content;
     }
     
+    private function deactivate_external_configs(){
+        $new_external_contents = array();
+        
+        $external_contents = OII_ECI_External_Content::get_by_post_id($this->post_id);
+        
+        if(count($external_contents)) {
+            foreach($external_contents AS $external_content) {
+                $external_content->active = false;
+                array_push($new_external_contents, $external_content->as_postmeta());
+            }
+        }
+        
+        if (count($new_external_contents)) 
+            update_post_meta($this->post_id, OII_ECI_Metabox::$meta_key, $new_external_contents);
+    }
 }
 
 
